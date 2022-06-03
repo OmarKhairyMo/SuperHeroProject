aa39df458ec948aaaed43813f99118db3d148922
Omar Khairy <79727362+OmarKhairyMo@users.noreply.github.com>
2022-06-03 10:29:35 +0200

    feat: Project Structure

---
+++ src/screens/RandomMovies.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const RandomMovies = () => {
+  return (
+    <View>
+      <Text>RandomMovies</Text>
+    </View>
+  );
+};
+
+export default RandomMovies;
+
+const styles = StyleSheet.create({});
